347919de1769e8e35b5a5f2449aee7ac308a1da5
sachinchaudhary1234 <anujchaudhary6397@gmail.com>
2026-09-14 14:12:41 +0530

    fix MongoStore import

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
+const MongoStore = require("connect-mongo");
+
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
