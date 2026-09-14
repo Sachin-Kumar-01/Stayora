7940a454207abdefff27b500cb431db5f660f9d7
sachinchaudhary1234 <anujchaudhary6397@gmail.com>
2026-09-14 14:18:27 +0530

    fix some errors

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const MongoStore = require("connect-mongo");
+
 
 
 const listingRouter = require("./routes/listing.js");
@@ -26,7 +27,7 @@ const userRouter = require("./routes/user.js");
 // const MONGO_URL = "mongodb://127.0.0.1:27017/Stayora";
 const dbUrl=process.env.ATLASDB_URL;
 
-main()
+main() 
     .then(() => {
         console.log("connected to DB");
     })
@@ -48,7 +49,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 const store = MongoStore.create({
     mongoUrl:dbUrl,
     crypto:{
-        secrete: process.env.SECRET,
+        secret: process.env.SECRET,
     },
     touchAfter: 24 * 3600,
 });
